a96c376ffe8078665b3099144dab5fd7e5d200f7
boiv66 <vothanhboiloc2409@gmail.com>
2022-06-07 17:00:18 +0700

    fix local storage overwrite when refresh page

--- app/controllers/v2/main-v2.js
+++ app/controllers/v2/main-v2.js
@@ -1,13 +1,16 @@
 const foodTable = document.getElementById("tbodyFood"); 
 
 // Get the current food list in local storage 
-const foodList = JSON.parse(localStorage.getItem("foodList")); 
+// const foodList = JSON.parse(localStorage.getItem("foodList")); 
 
 function renderFoodList(){
-    for (const item of foodList){
+
+    for (const item of Object.keys(localStorage)){
         //create the element
+        const itemValue =  JSON.parse(localStorage.getItem(item)); 
+        // console.log(itemValue); 
         const foodElement = document.createElement('tr'); 
-        insertRow(foodElement, item);  
+        insertRow(foodElement, itemValue);  
     
         //add to the table
         foodTable.appendChild(foodElement); 
@@ -18,6 +21,8 @@ function renderFoodList(){
 
 function insertRow(foodEl, item){
     //add value of item in cell 
+    console.log(item, 1); 
+    console.log(item.foodId, 123)
     foodEl.insertCell(-1).innerHTML = item.foodId; 
     foodEl.insertCell(-1).innerHTML = item.foodName; 
     foodEl.insertCell(-1).innerHTML = item.foodCategory;
